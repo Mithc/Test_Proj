ac04a1b1d03c6ab67e2b8bf47f8eba2a2d0cab58
Unknown <isha23@yandex.ru>
2016-12-06 00:08:23 +0300

    One more change
    Added a new one welcome

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-alert('Hello JavaScript!2');
+function main() {
+  $('.skillset').hide();
+  $('.skillset').fadeIn(1000);
+  $('.welcome').hide();
+  $('.welcome').fadeIn(300);
+
+  $('.projects').hide();
+
+  $('.projects-button').on('click', function() {
+		$(this).next().slideToggle(400);
+    $(this).toggleClass('active');
+    $(this).text('Projects Viewed');
+	});
+  $('.welcome-button').on('click', function() {
+      $('.welcome').fadeOut(500);
+  });
+}
+
+$(document).ready(main);
